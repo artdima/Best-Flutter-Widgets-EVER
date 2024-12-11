503e3a5fcbd55c5576efbc229e45089d74a24eb6
Medyannik Dmitriy <mail@artdima.ru>
2024-12-11 22:26:33 +0300

    Add AnimatedPadding

--- lib/screens/animated_padding.dart
+++ lib/screens/animated_padding.dart
@@ -7,20 +7,45 @@ class AnimatedPaddingExample extends StatefulWidget {
   State<AnimatedPaddingExample> createState() => _AnimatedPaddingExampleState();
 }
 
+//AnimatedPadding — это виджет во Flutter, который позволяет анимировать
+//изменение отступов (padding) вокруг дочернего виджета. Он полезен, если
+//нужно плавно изменять расстояние между виджетами в зависимости от состояния приложения.
 class _AnimatedPaddingExampleState extends State<AnimatedPaddingExample> {
   double padValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Change padding'),
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.orangeAccent),
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Padding Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  padValue = padValue == 0 ? 100.0 : 0.0;
+                });
+              },
+              child: const Text('Change padding'),
+            ),
+            Text('Paddin value $padValue'),
+            const SizedBox(height: 20),
+            AnimatedPadding(
+              padding: EdgeInsets.all(padValue),
+              duration: const Duration(seconds: 2),
+              curve: Curves.easeInOut,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 4,
+                color: Colors.orange,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
